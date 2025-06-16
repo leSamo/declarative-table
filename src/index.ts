e356e32995423a2ded73b6ec5f76eb45b529d7df
leSamo <soleksak@redhat.com>
2025-06-17 00:26:09 +0200

    Build config changes

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import DeclarativeTable from './DeclarativeTable/DeclarativeTable';
+import * as hooks from './DeclarativeTable/hooks';
+
+export {
+    DeclarativeTable,
+    hooks,
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'DeclarativeTable',
+      fileName: (format) => `declarativeTable.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
