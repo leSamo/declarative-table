f5b72828bcb57711fbfe22ed0b61b278b95af8e9
leSamo <soleksak@redhat.com>
2025-08-08 21:30:38 +0200

    Ability to clear table items

--- src/DeclarativeTable/DeclarativeTable.tsx
+++ src/DeclarativeTable/DeclarativeTable.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-// @ts-ignore
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
 import DeclarativeTableBody from './DeclarativeTableBody';
 import DeclarativeTableToolbar, { DeclarativeTableBulkAction, DeclarativeTableMeta, DeclarativeTableRow } from './DeclarativeTableToolbar';
 import DeclarativeTableFooter from './DeclarativeTableFooter';
@@ -15,7 +14,7 @@ export type DeclarativeTableColumn = Omit<ColumnManagementModalColumn, 'isShownB
   isShownByDefault?: boolean,
   dataLabel?: string,
   sortParam?: string,
-  sortDefaultDirection?: 'asc' | 'desc',
+  sortDefaultDirection?: 'asc' | 'desc',
   width?: number,
 }
 
@@ -39,14 +38,14 @@ interface DeclarativeTableProps {
   rowActions?: IAction[],
 }
 
-const DeclarativeTable = ({
+const DeclarativeTable = forwardRef(({
   isLoading,
   isExpandable,
   isSelectable,
   areColumnsManageable,
   rows,
   columns,
-  filterConfig = { items: []},
+  filterConfig = { items: [] },
   activeFiltersConfig = {},
   meta,
   errorStatus,
@@ -57,7 +56,7 @@ const DeclarativeTable = ({
   fetchBulk,
   bulkActions,
   rowActions,
-}: DeclarativeTableProps) => {
+}: DeclarativeTableProps, ref) => {
   const { offset, limit, total_items, sort } = meta;
 
   const [selectedRows, setSelectedRows] = useState({});
@@ -66,6 +65,10 @@ const DeclarativeTable = ({
     columns,
     (columns) => applyColumns?.(columns)
   );
+
+  useImperativeHandle(ref, () => ({
+    clearSelectedRows: () => setSelectedRows({})
+  }));
 
   return (
     <ErrorHandler errorStatus={errorStatus}>
@@ -124,7 +127,7 @@ const DeclarativeTable = ({
         apply={apply}
       />
     </ErrorHandler>
-  );
-};
+  )
+});
 
 export default DeclarativeTable;
